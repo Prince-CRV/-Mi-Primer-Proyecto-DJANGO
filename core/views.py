f0216e34751435a223c9079cdd5a62ab0881d8b2
Luis <playpassprince@gmail.com>
2023-08-13 12:38:56 +0100

    ..

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def myfirstview(request):
+    date = {
+        'name': 'Prince'
+    }
+    return JsonResponse(date)
