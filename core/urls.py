616d57c3201972864aa55ff02e53793767b2c69a
Luis <playpassprince@gmail.com>
2023-08-13 17:07:30 +0100

    ..

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from core.views import myfirstview
+from core.views import myfirstview, mysecondview
 
+app_name = 'core'
 
 urlpatterns = [
-    path('uno/',myfirstview),
-    path('dos/',myfirstview)
+    path('uno/',myfirstview,name='vista1'),
+    path('dos/',mysecondview,name='vista2')
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,18 @@
-from django.http import HttpResponse, JsonResponse
+from core.models import Category, Product
 from django.shortcuts import render
 
 # Create your views here.
 def myfirstview(request):
     date = {
-        'name': 'Prince'
+        'name': 'Prince',
+        'categories': Category.objects.all()
     }
-    return JsonResponse(date)
+    #return JsonResponse(date)
+    return render(request, 'index.html', date)
+
+def mysecondview(request):
+    date = {
+        'name': 'Prince',
+        'products': Product.objects.all()
+    }
+    return render(request, 'second.html', date)
