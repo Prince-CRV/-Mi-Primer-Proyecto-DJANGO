45b630c20813f6b4b16dcecc0796ce24a1ca4afc
Luis <playpassprince@gmail.com>
2023-08-14 17:31:44 +0100

    update

--- core/view/category/views.py
+++ core/view/category/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.views.generic import ListView
 from django.utils.decorators import method_decorator
 from core.models import Category
@@ -22,7 +23,7 @@ class CategoryListView(ListView):
     # def get_queryset(self):
     #     return Category.objects.filter(name__startswith='L')
 
-    #@method_decorator(login_required)
+    @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
