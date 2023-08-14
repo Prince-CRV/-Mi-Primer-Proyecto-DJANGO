790fb97800fa7d9297dc1aa0439bf26f1aebb2a4
Luis <playpassprince@gmail.com>
2023-08-14 16:51:31 +0100

    updated

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ app_name = 'core'
 
 urlpatterns = [
     #path('home/',myfirstview,name='home'),
-    path('category/list/', CategoryListView.as_view(), name='category_list')
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
+    path('category/list2/', category_list, name='category_list2')
 ]
--- core/view/category/views.py
+++ core/view/category/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
-
+from django.utils.decorators import method_decorator
 from core.models import Category
 
 
@@ -19,6 +21,14 @@ class CategoryListView(ListView):
 
     # def get_queryset(self):
     #     return Category.objects.filter(name__startswith='L')
+
+    #@method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {'name': 'Prince'}
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
