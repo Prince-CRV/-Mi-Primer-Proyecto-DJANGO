6456d6e4300d14dd52f58799afbcf4a8b16e7e91
Luis <playpassprince@gmail.com>
2023-08-14 10:25:30 +0100

    updated

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from core.views.category.views import category_list
+
+from core.view.category.views import category_list
 #from core.views import myfirstview
  
 
@@ -7,5 +8,5 @@ app_name = 'core'
 
 urlpatterns = [
     #path('home/',myfirstview,name='home'),
-    path('category/list/',category_list,name='category_list')
+    path('category/list/', category_list, name='category_list')
 ]
--- core/view/category/views.py
+++ core/view/category/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def category_list(request):
     data = {
-        
+
     }
-    
+
     return render(request, 'category/category_list.html', data)
