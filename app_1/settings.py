b7cc94f2eba4462aa4501b2ea7628c65c5769bc2
Luis <playpassprince@gmail.com>
2023-08-18 10:30:19 +0100

    update

--- app_1/settings.py
+++ app_1/settings.py
@@ -40,10 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Libs
+    'widget_tweaks',
+    'fontawesomefree',
     # Apps
     'core',
-    'fontawesomefree',
-    
     
 ]
 
