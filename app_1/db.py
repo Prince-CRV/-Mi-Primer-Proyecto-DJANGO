b21046302159c13e927201a2b936193175877865
Luis <playpassprince@gmail.com>
2023-08-21 17:41:19 +0100

    update

--- app_1/db.py
+++ app_1/db.py
@@ -4,13 +4,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SQLITE = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db/sqlite/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
 POSTGRESQL = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db',
         'USER': 'postgres',
         'PASSWORD': 'CLEOPATRa98.',
--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,6 @@ app_name = 'core'
 urlpatterns = [
     #path('home/',myfirstview,name='home'),
     path('category/list/', CategoryListView.as_view(), name='category_list'),
-    path('category/list2/', category_list, name='category_list2'),
     path('category/add/', CategoryCreateView.as_view(), name='category_create'),
     path('category/update/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
     path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
