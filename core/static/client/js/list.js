fe745e8b69468bbcc432e7a8b171e809e4e89a5d
Prince <freefireprince211098@gmail.com>
2023-08-29 20:43:46 +0100

    update

--- core/static/client/js/list.js
+++ core/static/client/js/list.js
@@ -30,8 +30,8 @@ function getData() {
                 class: 'text-center',
                 orderable: false,
                 render: function (data, type, row) {
-                    var buttons = '<a href="#" rel="edit" class="btn btn-outline-warning btn-flat btnEdit"><i class="fa-solid fa-pen-to-square"></i></a> ';
-                    buttons += '<a href="#" type="button" class="btn btn-outline-danger btn-flat"><i class="fa-solid fa-trash"></i></a>';
+                    var buttons = '<a href="#" rel="edit" class="btn btn-outline-warning btn-flat btnEdit"><i class="fa-solid fa-pen-to-square" style="color: #000000;"></i></a> ';
+                    buttons += '<a href="#" rel="delete" class="btn btn-outline-danger btn-flat"><i class="fa-solid fa-trash" style="color: #000000;"></i></a>';
                     return buttons;
                 }
             },
@@ -57,21 +57,33 @@ $(function () {
         $('#myModalClient').modal('show');
     });
 
-    $('#data tbody').on('click', 'a[rel="edit"]', function () {
-        modal_title.find('span').html('Edición de un cliente');
-        modal_title.find('i').removeClass().addClass('fa-solid fa-edit fa-beat');
-        var tr = tblClient.cell($(this).closest('td, li')).index();
-        var data = tblClient.row(tr.row).data();
-        $('input[name="action"]').val('edit');
-        $('input[name="id"]').val(data.id);
-        $('input[name="names"]').val(data.names);
-        $('input[name="surnames"]').val(data.surnames);
-        $('input[name="dni"]').val(data.dni);
-        $('input[name="date_birthday"]').val(data.date_birthday);
-        $('input[name="address"]').val(data.address);
-        $('select[name="gender"]').val(data.gender.id);
-        $('#myModalClient').modal('show');
-    });
+    $('#data tbody')
+        .on('click', 'a[rel="edit"]', function () {
+            modal_title.find('span').html('Edición de un cliente');
+            modal_title.find('i').removeClass().addClass('fa-solid fa-edit fa-beat');
+            var tr = tblClient.cell($(this).closest('td, li')).index();
+            var data = tblClient.row(tr.row).data();
+            $('input[name="action"]').val('edit');
+            $('input[name="id"]').val(data.id);
+            $('input[name="names"]').val(data.names);
+            $('input[name="surnames"]').val(data.surnames);
+            $('input[name="dni"]').val(data.dni);
+            $('input[name="date_birthday"]').val(data.date_birthday);
+            $('input[name="address"]').val(data.address);
+            $('select[name="gender"]').val(data.gender.id);
+            $('#myModalClient').modal('show');
+        })
+        .on('click', 'a[rel="delete"]', function () {
+            var tr = tblClient.cell($(this).closest('td, li')).index();
+            var data = tblClient.row(tr.row).data();
+            var parameters = new FormData();
+            parameters.append('action', 'delete');
+            parameters.append('id', data.id);
+            submit_with_ajax(window.location.pathname, 'Notificación', '¿Estas seguro de realizar eliminar el siguiente registro?', parameters, function () {
+                tblClient.ajax.reload();
+            });
+        });
+
 
     $('#myModalClient').on('shown.bs.modal', function () {
         //$('form')[0].reset();
