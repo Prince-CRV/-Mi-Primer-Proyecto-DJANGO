ca9419ac802dcfee1101e531f1a7dce9cf7646a0
Prince <freefireprince211098@gmail.com>
2023-08-31 22:09:08 +0100

    update

--- core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from core.models import *
-
-# Register your models here.
-admin.site.register(Category)
-# Register your models here.
--- run_tests.py
+++
@@ -1,3 +0,0 @@
-from core.tests import *
-
-# ok
