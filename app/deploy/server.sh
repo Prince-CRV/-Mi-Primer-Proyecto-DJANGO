0e19704ef14426489bcf6e2c0b6932ede42fe6b0
PrinceC6 <playpassprince@gmail.com>
2023-09-18 19:05:18 +0000

    update

--- app/deploy/server.sh
+++ app/deploy/server.sh
@@ -2,6 +2,6 @@
 DJANGODIR=$(dirname $(cd `dirname $0` && pwd))
 DJANGO_SETTINGS_MODULE=config.settings
 cd $DJANGODIR
-source env/bin/activate
+source venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 exec python manage.py runserver 0:8000
