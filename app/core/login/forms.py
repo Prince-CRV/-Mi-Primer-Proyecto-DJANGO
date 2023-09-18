aab1b3c6f77af3e3933f87a67d7f3753a4bdb356
Prince <freefireprince211098@gmail.com>
2023-09-18 12:28:26 +0100

    updated

---
+++ app/core/login/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+
+from core.user.models import User
+
+
+class ResetPasswordForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Ingrese su username',
+        'autocomplete': 'off',
+    }))
+
+    def clean(self):
+        cleaned = super().clean()
+        if not User.objects.filter(username=cleaned['username']).exists():
+            # self._errors['error'] = self._errors.get('error', self.error_class())
+            # self._errors['error'].append('El usuario no existe')
+            raise forms.ValidationError('El usuario no existe')
+        return cleaned
+
+    def get_user(self):
+        username = self.cleaned_data.get('username')
+        return User.objects.get(username=username)
+
+
+class ChangePasswordForm(forms.Form):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Ingrese un password',
+        'autocomplete': 'off',
+    }))
+
+    confirmPassword = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Repita el password',
+        'autocomplete': 'off',
+    }))
+
+    def clean(self):
+        cleaned = super().clean()
+        password = cleaned['password']
+        confirmPassword = cleaned['confirmPassword']
+        if password != confirmPassword:
+            # self._errors['error'] = self._errors.get('error', self.error_class())
+            # self._errors['error'].append('El usuario no existe')
+            raise forms.ValidationError('Las contraseñas deben ser iguales')
+        return cleaned
---
+++ app/core/user/static/user/js/list.js
@@ -0,0 +1,61 @@
+$(function () {
+    $('#data').DataTable({
+        //responsive: true,
+        scrollX: true,
+        autoWidth: false,
+        destroy: true,
+        deferRender: true,
+        ajax: {
+            url: window.location.pathname,
+            type: 'POST',
+            data: {
+                'action': 'searchdata'
+            },
+            dataSrc: ""
+        },
+        columns: [
+            {"data": "position"},
+            {"data": "full_name"},
+            {"data": "username"},
+            {"data": "date_joined"},
+            {"data": "image"},
+            {"data": "groups"},
+            {"data": "id"},
+        ],
+        columnDefs: [
+            {
+                targets: [-3],
+                class: 'text-center',
+                orderable: false,
+                render: function (data, type, row) {
+                    return '<img src="' + row.image + '" class="img-fluid mx-auto d-block" style="width: 20px; height: 20px;">';
+                }
+            },
+            {
+                targets: [-2],
+                class: 'text-center',
+                orderable: false,
+                render: function (data, type, row) {
+                    var html = '';
+                    $.each(row.groups, function (key, value) {
+                        html += '<span class="badge badge-success">' + value.name + '</span> ';
+                    });
+                    return html;
+                }
+            },
+            {
+                targets: [-1],
+                class: 'text-center',
+                orderable: false,
+                render: function (data, type, row) {
+                    var buttons = '<a href="/user/update/' + row.id + '/" class="btn btn-outline-warning btn-xs btn-flat"><i class="fa-solid fa-edit" style="color: #000000;"></i></a> ';
+                    buttons += '<a href="/user/delete/' + row.id + '/" type="button" class="btn btn-outline-danger btn-xs btn-flat"><i class="fa-solid fa-trash" style="color: #000000;"></i></a>';
+                    return buttons;
+                }
+            },
+        ],
+        initComplete: function (settings, json) {
+
+        }
+    });
+});
