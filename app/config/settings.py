64587b945e1aeea60a46a738848cb58a17691f31
Prince <freefireprince211098@gmail.com>
2023-09-20 21:08:43 +0100

    updated

--- app/config/settings.py
+++ app/config/settings.py
@@ -32,7 +32,7 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-CSRF_TRUSTED_ORIGINS = ['princesoft.es']
+CSRF_TRUSTED_ORIGINS = ['www.princesoft.es']
 
 # Application definition
 
